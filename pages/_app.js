aa86be5c390a5d4cfb68b8a84ef015a3fe7b3c9a
pratik-TA <pratik.padhiyar@techalmas.com>
2022-12-09 18:20:34 +0530

    routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import { useEffect } from "react";
+import Header from "../ui/header";
+import Footer from "../ui/footer";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
-  return <Component {...pageProps} />;
+  return <>
+    <Header />
+    <Component {...pageProps} />
+    <Footer />
+  </>
 }
 
 export default MyApp;
--- ui/header/index.jsx
+++ ui/header/index.jsx
@@ -1,8 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
   return (
+    <>
     <div>Header</div>
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/about-us">About Us</Link>
+      </li>
+      <li>
+        <Link href="/blog">Blog</Link>
+      </li>
+    </ul>
+    </>
   )
 }
 
